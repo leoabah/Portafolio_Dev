ae01c0088b2795b2d3919404bfb2525609eb85ab
leoabah <50350589+leoabah@users.noreply.github.com>
2026-07-22 12:56:16 -0300

    fix: update Experience component to include additional steps and correct class name

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -15,6 +15,41 @@ const  steps =[
         number:"03",
         title:"Integración de APIs",
         text:"Conexión eficiente con servicios externos para enriquecer la funcionalidad de las aplicaciones."
+    },
+    {
+        number:"04",
+        title:"Testing y Depuración",
+        text:"Aseguramiento de la calidad del software mediante pruebas exhaustivas y resolución de problemas."
+    },
+    {
+        number:"05",
+        title:"Optimización de Rendimiento",
+        text:"Mejora de la velocidad y eficiencia de las aplicaciones para una experiencia de usuario fluida."
+    },
+    {
+        number:"06",
+        title:"Mantenimiento y Soporte",
+        text:"Brindar atención continua y soluciones efectivas para garantizar el funcionamiento óptimo de las aplicaciones."
+    },
+    {
+        number:"07",
+        title:"Desarrollo Backend",
+        text:"Creación y mantenimiento de servidores y bases de datos para soportar las aplicaciones frontend."
+    },
+    {
+        number:"08",
+        title:"Seguridad y Autenticación",
+        text:"Implementación de medidas de seguridad para proteger los datos y la privacidad de los usuarios."
+    },
+    {
+        number:"09",
+        title:"Manejo de bases de datos No SQL",
+        text:"Gestión eficiente de datos no relacionales utilizando tecnologías como MongoDB o DynamoDB."
+    },
+    {
+        number:"10",
+        title:"Despliegue en producción y escalabilidad",
+        text:"Implementación de aplicaciones en entornos de producción y planificación para manejar el crecimiento del tráfico y la demanda."
     }
 ];
 
@@ -23,7 +58,7 @@ function Experience() {
         <section className="experience">
             <div className="container">
                 <motion.div
-                clasName="sectionTiltle"
+                className="sectionTitle"
                 initial={{opacity:0,y:40}}
                 whileInView={{opacity:1,y:0}}
                 transition={{duration:0.6}}
